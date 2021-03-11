90f19bcebdf217e9e56c6459f3ff16b7f8e1c383
LowercaseGuy <48055178+VenuGopal1899@users.noreply.github.com>
2021-03-11 11:49:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello Venu! Guess you are using docker for first time');
 });
 
 var server = app.listen(3000, function () {
